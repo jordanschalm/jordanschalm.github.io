3913265ea4f02a7a119a93d081a669f495d6507c
Jordan Schalm <jordan.schalm@gmail.com>
2019-02-02 17:39:49 -0800

    update copy

--- src/data.js
+++ src/data.js
@@ -18,7 +18,7 @@ export default {
           blockchain-based experiences that are made for you and ready for the
           real world. We aim to bring the benefits of decentralization to the
           first billion consumers through the power of play, fairness, and true
-          ownership. `,
+          ownership.`,
       },
       {
         name: 'CryptoKitties',
@@ -29,16 +29,15 @@ export default {
           start: Moment('2017-07', MONTH_F),
         },
         description: `CryptoKitties is the world's most successful blockchain game.
-          It has generated over $25M in P2P sales and has been covered in the 
+          It has generated over $25M in P2P sales and has been covered in the
           [BBC](http://www.bbc.com/news/technology-42237162),
           [New York Times](https://www.nytimes.com/2017/12/28/style/cryptokitties-want-a-blockchain-snuggle.html),
           [Wall Street Journal](https://blogs.wsj.com/cio/2017/12/07/ethereum-network-copes-with-surge-of-activity-as-virtual-kitten-game-goes-viral/),
           and [many others](https://cryptokitties.co/press).
-          I prototyped early versions of the game and helped to model and
-          develop cat population mechanics and the genetics system. I also
-          built the [smart contracts](https://www.gettoby.com/p/448v6x9p6tgs) 
-          for the auction system of the final game.`,
-      },
+          I built the [smart contracts](https://www.gettoby.com/p/448v6x9p6tgs) for
+          the P2P auction system of the final game and helped to model kitty population
+          and genetic mechanics.`,
+       },
     ],
     older: [
       {
@@ -51,13 +50,14 @@ export default {
           end: Moment('2018-04', MONTH_F),
         },
         description: `Axiom Zen is a venture studio based in Vancouver, Canada.
-          I worked primarily with blockchain initiatives like CryptoKitties and
-          investigating scaling solutions, with the goal of making blockchain
-          technology more accessible and fun.`,
+          I worked on several projects, including
+          [Toby](https://www.gettoby.com/), an indispensable tab-taming Chrome
+          extension, and [CryptoKitties](https://cryptokitties.co), the world's
+          most successful blockchain game.`
       },
       {
         name: 'BASc Computer Engineering',
-        subheader: 'Learning!',
+        subheader: 'Learning',
         url: 'https://ubc.ca',
         emoji: '🎓',
         date: {
@@ -77,17 +77,17 @@ export default {
           end: Moment('2018-05', MONTH_F),
         },
         description: `Launch Pad is [UBC's](https://www.ubc.ca/) largest
-          student-run software engineering club. I led the executive team to 
+          student-run software engineering club. I led the executive team to
           standardize engineering process and streamline recruitment in service
           of increasing the organization's ability to support more members and
           enabling those members to build skills more quickly. Some cool
-          projects we've built since then include a [speaker diarization API](https://github.com/ubclaunchpad/minutes), 
+          projects we've built since then include a [speaker diarization API](https://github.com/ubclaunchpad/minutes),
           a [self-hosted continuous deployment tool](https://github.com/ubclaunchpad/inertia),
           and a [collaborative Etch-a-Sketch](https://github.com/ubclaunchpad/etch.io).`,
       },
       {
         name: 'TEDxUBC',
-        subheader: 'Talking about cats',
+        subheader: 'Feline Discussions',
         url: 'https://www.youtube.com/watch?v=29W67cSPnRc',
         emoji: '📽',
         date: Moment('2018-03-03', DATE_F),
@@ -99,17 +99,17 @@ export default {
       },
       {
         name: 'ETHWaterloo',
-        subheader: 'Hackathon',
+        subheader: 'BUIDL',
         url: 'https://ethwaterloo.com/',
         emoji: '🔨',
         date: Moment('2017-10-15', DATE_F),
         description: `ETHWaterloo was the world's largest Ethereum hackathon,
-          with over 400 participants. My team 
+          with over 400 participants. My team
           [won](https://www.newswire.ca/news-releases/cats-invade-worlds-largest-ethereum-hackathon-651263343.html)
           with [Rufflet](https://rufflet.cryptokitties.co/), a Mixpanel-style
           analytics tool for smart contracts. At the same time we were
           polishing off the beta version of CryptoKitties and releasing it for
-          the hackers at ETHWaterloo to 
+          the hackers at ETHWaterloo to
           [play with](https://motherboard.vice.com/en_us/article/bj78jv/cryptokitties-blockchain-cats-axiom-zen-game).`,
       },
       {
@@ -122,7 +122,7 @@ export default {
           end: Moment('2017-08', MONTH_F),
         },
         description: `Rocket is a Slack bot I built to improve onboading and
-          project management at [Launch Pad](https://ubclaunchpad.com). 
+          project management at [Launch Pad](https://ubclaunchpad.com).
           New members add their information by talking to Rocket in Slack.
           From there, Rocket automatically updates our website roster, adds
           them to our GitHub organization, and allows team leads to manage
@@ -130,7 +130,7 @@ export default {
       },
       {
         name: 'Cumulus',
-        subheader: 'Blockchain investigations',
+        subheader: 'Blockchain ',
         url: 'https://github.com/ubclaunchpad/cumulus',
         emoji: '💸',
         date: {
@@ -139,9 +139,9 @@ export default {
         },
         description: `Cumulus is an open-source cryptocurrency implementation
           modelled after Bitcoin and Ethereum that I built with a group of
-          friends at [Launch Pad](https://ubclaunchpad.com) during the summer
-          of 2017. Everything from the blockchain, peer-to-peer networking, and
-          consensus was built from scratch (which was a lot of fun).`,
+          friends at [Launch Pad](https://ubclaunchpad.com). Everything from
+          the blockchain, peer-to-peer networking, and consensus was built
+          from the ground up.`,
       },
       {
         name: 'Guerrilla',
@@ -151,7 +151,7 @@ export default {
         description: `Guerrilla is an open-source, extensible SMTP mail server
           written in Go. I developed a real-time analytics dashboard for
           monitoring mail traffic and helped modularize the package to make it
-          usable as a component of other projects.`,
+          reusable as a component of other projects.`,
       },
       {
         name: 'Eight Solutions',
@@ -179,7 +179,7 @@ export default {
         emoji: '📰',
         description: `The Ubyssey is UBC's student newspaper and the largest
           student newspaper in Western Canada. While working there I helped
-          build [Dispatch](https://github.com/ubyssey/dispatch), the 
+          build [Dispatch](https://github.com/ubyssey/dispatch), the
           open-source publishing platform that powers
           [ubyssey.ca](https://ubyssey.ca).`,
       },
